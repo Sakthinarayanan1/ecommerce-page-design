4089ac62052f9c6e412347c33e5f5c376d85bf03
Sakthinarayanan1 <sakthinarayanankp@gmail.com>
2023-10-09 18:36:10 +1345

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+<html>
+    <head></head>
+<body>
+
+<script>
+
+
+
+let salaries=[55000,65000,95000,75000,45000]
+for (let element of salaries) {
+if(element>60000)
+  console.log(element);
+}
+</script>
+</body>
+</html>
---
+++ signin.js
@@ -0,0 +1,19 @@
+// $(".tab-content").hide();
+// $(".tab-content:first").show();
+// $("#form ul li").click(function() {
+
+//   $("#form ul li").removeClass("active");
+//   $(this).addClass("active");
+//   if ($(this).hasClass('slideline')) {
+//     return;
+//   }
+//   var whatTab = $(this).index();
+//   var howFar = 135 * whatTab;
+//   $(".slideline").css({
+//     left: howFar + "px"
+//   });
+//   $(".tab-content").hide();
+//   var activeTab = $(this).attr("rel");
+//   $("#" + activeTab).fadeIn(500);
+// });
+
